87dc867f273a1876fb18eccf70483f74d6d1f7a8
viditjain08 <viditjain08@gmail.com>
2017-11-28 15:13:20 +0530

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-#Transitive#
+                                      #Transitive#
+
+
+Q1. Write a Prolog program for testing whether a given (binary) relation R (on a finite set S) is
+transitive.
+
+ INPUT FORMAT 
+		(a) checkTransitive(S,R).
+		
+		where S is the finite set and R is the binary relation on set S
+		example : checkTransitive([1,2,3],[[1,2],[1,3]]).
+
+
+Q2. Add rules (or modify the program in Q1) to compute the transitive closure of a given relation.
+
+
+ INPUT FORMAT
+		(b) closure(S,R).
+
+		where S is the finite set and R is the binary relation on set S
+		example : closure([1,2,3],[[1,2],[1,3]]).
--- abc.pl
+++ abc.pl
@@ -1,41 +1,49 @@
+/*   NAME - VIDIT JAIN  , ID - 2016A7PS0064P
+ *   NAME - PIYUSH GARG , ID - 2016A7PS0035P  */
+/*   PLEASE REFER TO THE README FILE FOE THE INPUT  */
+
+
+trans([X,Y],[Y,X],[X,X]).
+trans([_,_],[_,_],A):-
+	A=[],!.
 transitive([W,X],[Y,Z],A):-
 	X=\=Y,
 	W=\=Z,
 	A=[],!.
-trans([X,Y],[Y,X],[X,X]).
-trans([W,X],[Y,Z],A):-
-	A=[],!.
 transitive([Y,X],[Z,Y],[Z,X]).
 transitive([X,Y],[Y,Z],[X,Z]).
 
-checktransitive(S,R):-
+%Checking Transitive Relation
+checkTransitive(S,R):-
 	def(R,R,R,G1),
-	%write(G1),
 	length(R)==length(G1).
+
+%Finding Transitive Closure
 closure(S,R):-
 	def(R, R, R, G1),
 	length(G1,X),
 	write("Transitive Closure="),
 	print(G1,X).
+
+%Creating Print Function
 print([],0).
 print([H|T],C):-
 	C1 is C-1,
 	write(H),
-	print(T,C1).	
-%abc([],_,G,G).
-%abc(_,[],G,G).
+	print(T,C1).
 
+%Function to Append Lists
 append1(A,X,[],X,A).
 append1(A,X,Y,Z,B):-
-	memberfunc(Y,X,G,C),
-	%write(G),
+	memberfunc(Y,X,G,_),
 	(G==0,
 	append(X,[Y],Z),
 	append(A,[Y],B)),!;
 	Z=X,
 	B=A.
-	
-memberfunc(H,[],G,X):-
+
+%Creating Membership Relation
+memberfunc(_,[],G,X):-
 	X=[],
 	G=0.
 memberfunc(H, [H1|T1],G,X):-
@@ -44,29 +52,42 @@ memberfunc(H, [H1|T1],G,X):-
 	X=T1),!;
 	memberfunc(H,T1, G,X),!.
 
+% Compares all the pairs of elements one by one with all the input pairs
+% one by one and finds the new pair of elements for the transitive
+% closure and updates in G2.
 
 def(_,[],G2,G2).
 def(R, [H|T],G1,G2):-
-	abc(R,[H|T],G1,G3),
-	%write(G3),nl,
-	memberfunc(H,G3,G,X),
-	%write(X),nl,
+	find(R,[H|T],G1,G3),
+	memberfunc(H,G3,_,X),
 	def(G3,X,G3,G2),!.
 
-abc([],_,G2,G2).
-abc([H1|T1], [H|T], G1, G2):-
-	%write(H),
-	%write(H1),
+% Compares 1 pair of element stored in H with all the input pairs one by
+% one and finds the new pair of elements for the transitive closure and
+% updates in G3.
+
+find([],_,G2,G2).
+find([H1|T1], [H|T], G1, G2):-
 	trans(H,H1,X),
-	%write(X),nl,
 	append1(T1,G1,X,G4,T3),
 	transitive(H,H1,Z),
-	%write(H),
-	%write(H1),
-	%write(Z),nl,
-	%write(T3),
 	append1(T3, G4, Z, G3, T2),
-	%write(G3),nl,
-	abc(T2, [H|T], G3, G2),!. 
+	find(T2, [H|T], G3, G2),!.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
